c4c98dde49b165e9dbbac7f91da65fac16ea605f
7799043 <leoniak.maciek1@gmail.com>
2023-10-23 14:19:06 +0200

    sort option

--- pages/SortPage.ts
+++ pages/SortPage.ts
@@ -1,20 +1,34 @@
 import { Page } from '@playwright/test';
 
 export class SortPage {
-    page: Page; 
+    page: Page;
+
+    readonly sortOptionSelector = '[data-test="product_sort_container"]';
+    readonly selectOptionAtoZ = 'az';
+    readonly selectOptionZtoA = 'za';
+    readonly selectOptionPriceHighToLow = 'hilo';
+    readonly selectOptionPriceLowToHigh = 'lohi';
 
     constructor(page: Page) {
         this.page = page;
     }
 
-    async sort() {
-      
-
-        await this.page.locator('[data-test="product_sort_container"]').selectOption('za');
-        await this.page.locator('[data-test="product_sort_container"]').selectOption('lohi');
-        await this.page.locator('[data-test="product_sort_container"]').selectOption('hilo');
-        
-
-        
+    async sortByNameAtoZ() {
+        await this.page.locator(this.sortOptionSelector).selectOption(this.selectOptionAtoZ);
     }
+
+    async sortByNameZtoA() {
+        await this.page.locator(this.sortOptionSelector).selectOption(this.selectOptionZtoA);
+    }
+
+    async sortByPriceHightoLow() {
+        await this.page.locator(this.sortOptionSelector).selectOption(this.selectOptionPriceHighToLow);
+    }
+
+    async sortByPriceLowToHigh() {
+
+        await this.page.locator(this.sortOptionSelector).selectOption(this.selectOptionPriceLowToHigh);
+    }
+
+    
 }
--- tests/03_products_sort_by_name_feature_verification.spec.ts
+++ tests/03_products_sort_by_name_feature_verification.spec.ts
@@ -1,21 +1,36 @@
 import { test } from '@playwright/test';
 import { LoginPage } from '../pages/LoginPage';
+import { SortPage } from '../pages/SortPage';
 
 const username = 'standard_user';
 const password = 'secret_sauce';
 
 test.beforeEach(async ({ page }) => {
-    const loginPage = new LoginPage(page);
-    await loginPage.login(username, password);
+  const loginPage = new LoginPage(page);
+  await loginPage.login(username, password);
 });
 
 test.afterEach(async ({ page }) => {
-    await page.close()
+  await page.close()
 })
 
-test('....', async ({ page }) => {
-  await page.locator('[data-test="product_sort_container"]').selectOption('za');
-  await page.locator('[data-test="product_sort_container"]').selectOption('lohi');
-  await page.locator('[data-test="product_sort_container"]').selectOption('hilo');
+test.describe('sort option work properly for every select options', () => {
+  test('select option for...', async ({ page }) => {
+    const sortPage = new SortPage(page);
+    await sortPage.sortByPriceHightoLow();
 
+    const item = await page.locator('.inventory_item').nth(0).textContent();
+    const regex = /\s*(\d+(\.\d+)?)/g;
+    const item0 = item?.match(regex);
+    const item01 = parseFloat(item0 as unknown as string)
+    console.log(item01)
+
+    const item2 = await page.locator('.inventory_item').nth(1).textContent();
+    const item02 = item2?.match(regex);
+    const item002 = parseFloat(item02 as unknown as string)
+
+    console.log(item002)
+
+  })
+  
 });
